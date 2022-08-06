ebe51fccb1c8638488f4cfceda4df314c4fd755e
Paulo Simão <paulo@digitalcircle.com.br>
2022-08-06 12:16:54 -0300

    added crud xls mgr - allows usage of xls files to manipulate cruds

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/nats-io/nats.go v1.16.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.8.0
+	github.com/xuri/excelize/v2 v2.6.0
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	gorm.io/gorm v1.23.8
 )
@@ -34,6 +35,7 @@ require (
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/nats-io/jwt/v2 v2.2.1-0.20220330180145-442af02fd36a // indirect
 	github.com/nats-io/nats-server/v2 v2.8.2 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
@@ -41,8 +43,14 @@ require (
 	github.com/nats-io/stan.go v0.10.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.1 // indirect
+	github.com/xuri/efp v0.0.0-20220407160117-ad0f7a785be8 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	go.etcd.io/bbolt v1.3.6 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ lib/crudxlsmgr/crudxlsmgr.go
@@ -0,0 +1,170 @@
+package crudxlsmgr
+
+import (
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"gorm.io/gorm"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var db *gorm.DB
+
+func Setup(ndb *gorm.DB) {
+	db = ndb
+}
+
+type fieldHeader struct {
+	fname  string
+	ftype  string
+	format string
+}
+
+const (
+	OP_INSERT   = "i"
+	OP_UPDATE   = "u"
+	OP_DELETE   = "d"
+	TYPE_STRING = "string"
+	TYPE_INT    = "int"
+	TYPE_FLOAT  = "float"
+	TYPE_DATE   = "date"
+	TYPE_BOOL   = "bool"
+	COL_OP      = 0
+	COL_ID      = 1
+)
+
+func prepareHeader(cols []string) []fieldHeader {
+	ret := make([]fieldHeader, 0)
+	for i := 2; i < len(cols); i++ {
+		parts := strings.Split(cols[i], ",")
+		var fh = fieldHeader{
+			fname: parts[0],
+			ftype: TYPE_STRING,
+		}
+		if len(parts) > 1 {
+			fh.ftype = parts[1]
+		}
+		if len(parts) > 2 {
+			fh.format = parts[2]
+		}
+		ret = append(ret, fh)
+	}
+	return ret
+}
+
+func prepareUpsertVal(h []fieldHeader, row []string) (map[string]interface{}, error) {
+	var ret = map[string]interface{}{}
+	for i := 2; i < len(h)+2; i++ {
+		oneHeader := h[i-2]
+		switch oneHeader.ftype {
+		case TYPE_STRING:
+			ret[oneHeader.fname] = row[i]
+			continue
+		case TYPE_BOOL:
+			b, err := strconv.ParseBool(row[i])
+			if err != nil {
+				return nil, fmt.Errorf("field: %s - %s", oneHeader.fname, err.Error())
+			}
+			ret[oneHeader.fname] = b
+			continue
+		case TYPE_FLOAT:
+			b, err := strconv.ParseFloat(row[i], 64)
+			if err != nil {
+				return nil, fmt.Errorf("field: %s - %s", oneHeader.fname, err.Error())
+			}
+			ret[oneHeader.fname] = b
+			continue
+		case TYPE_INT:
+			b, err := strconv.ParseInt(row[i], 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("field: %s - %s", oneHeader.fname, err.Error())
+			}
+			ret[oneHeader.fname] = b
+			continue
+		case TYPE_DATE:
+			b, err := time.Parse(oneHeader.format, row[i])
+			if err != nil {
+				return nil, fmt.Errorf("field: %s - %s", oneHeader.fname, err.Error())
+			}
+			ret[oneHeader.fname] = b
+		}
+
+	}
+	return ret, nil
+}
+
+func processRow(h []fieldHeader, row []string, tb string) error {
+	switch row[0] {
+	case OP_INSERT:
+		upsert, err := prepareUpsertVal(h, row)
+		if err != nil {
+			return err
+		}
+		return db.Table(tb).Create(upsert).Error
+	case OP_UPDATE:
+		upsert, err := prepareUpsertVal(h, row)
+		if err != nil {
+			return err
+		}
+		return db.Table(tb).Where("id = ?", row[COL_ID]).Updates(upsert).Error
+	case OP_DELETE:
+		return db.Table(tb).Where("id = ?", row[COL_ID]).Delete(nil).Error
+	}
+
+	return nil
+}
+
+func processXls(xls *excelize.File, tb string) error {
+	sheetName := xls.GetSheetName(xls.GetActiveSheetIndex())
+	rows, err := xls.Rows(sheetName)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+	counter := 0
+	var headers []fieldHeader
+	if rows.Next() {
+		counter++
+		cols, err := rows.Columns()
+		if err != nil {
+			return err
+		}
+		headers = prepareHeader(cols)
+	}
+	for rows.Next() {
+		counter++
+		cols, err := rows.Columns()
+		if err != nil {
+			return err
+		}
+		err = processRow(headers, cols, tb)
+		if err != nil {
+			return fmt.Errorf("error processing row %v - %s", counter, err.Error())
+		}
+	}
+	return nil
+}
+
+func UploadExcel(w http.ResponseWriter, r *http.Request) error {
+	err := r.ParseMultipartForm(2 << 20)
+	if err != nil {
+		return err
+	}
+	for _, fs := range r.MultipartForm.File {
+		for _, f := range fs {
+			fdesc, err := f.Open()
+			if err != nil {
+				return err
+			}
+			xls, err := excelize.OpenReader(fdesc)
+			if err != nil {
+				return err
+			}
+			tb := r.URL.Query().Get("table")
+			err = processXls(xls, tb)
+		}
+	}
+	return nil
+}
